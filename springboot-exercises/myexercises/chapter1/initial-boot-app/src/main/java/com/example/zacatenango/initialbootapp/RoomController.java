3aa91286423b2ad2a69c1a5d81651555892d5b64
Zacatenango <zacatenango@github.com>
2022-10-24 15:01:24 -0500

    Explored the issue of view names

--- springboot-exercises/myexercises/chapter1/initial-boot-app/src/main/java/com/example/zacatenango/initialbootapp/RoomController.java
+++ springboot-exercises/myexercises/chapter1/initial-boot-app/src/main/java/com/example/zacatenango/initialbootapp/RoomController.java
@@ -10,9 +10,12 @@ import java.util.List;
 
 // To define a class as a Spring MVC controller, we annotate it with @Controller
 // We also have to give it a request mapping URL with @RequestMapping
-// According to Spring documentation, templates are automatically picked up from src/main/resources/templates according
-// to the servlet's mapping
-@Controller @RequestMapping("/rooms") public class RoomController
+// So we define 3 names in this view controller:
+// Name 1: the servlet URL, which is "roomlist", and will be used as the page's URL
+// Name 2: the name of our variable in the template context, through which we'll access our object
+// Name 3: the view's name, which will double as our template filename in src/main/resources/templates/<view name>.html
+// Right here we have Name 1, which is the view's URL
+@Controller @RequestMapping("/roomlist") public class RoomController
 {
    private static List<Room> rooms = new ArrayList();
 
@@ -36,8 +39,15 @@ import java.util.List;
    @GetMapping public String getAllRooms(Model model)
    {
       // Using model.addAttribute(), we add our rooms list object to the template context and call it "rooms"
+      // This is our Name 2.
       model.addAttribute("rooms", rooms);
-      // We return the name of our attribute
-      return "rooms";
+      // We return the name of our view. Spring will take this as the base name of our template, without ".html"
+      // This is Name 3.
+      return "room";
    }
 }
+
+// So, to wrap it up:
+// - roomlist is our URL, it is defined in the request mapping
+// - rooms is our rooms list in the template context
+// - room is our view name, and it corresponds to template src/main/resources/templates/room.html
