88889d295e747b0be819d35b3fbde15b283c79b2
Zacatenango <zacatenango@github.com>
2022-11-04 13:22:22 -0600

    Starting an users page exercise

--- springboot-exercises/myexercises/chapter1/initial-boot-app/src/main/java/com/example/zacatenango/initialbootapp/StaffPage.java
+++
@@ -1,4 +0,0 @@
-package com.example.zacatenango.initialbootapp;
-
-public class StaffPage {
-}
---
+++ springboot-exercises/myexercises/chapter1/initial-boot-app/src/main/java/com/example/zacatenango/initialbootapp/controllers/RestrictedPages.java
@@ -0,0 +1,12 @@
+package com.example.zacatenango.initialbootapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller public class RestrictedPages
+{
+   @GetMapping("/userzone") public String userzone()
+   {
+      return "userzone";
+   }
+}
